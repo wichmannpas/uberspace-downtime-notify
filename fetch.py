83e6c7555d877095d16e39cbdf16cd6552e833cd
Pascal Wichmann <wichmannpas@gmail.com>
2016-09-08 14:02:52 +0200

    Storing hashes of sent messages to prevent resending them on each script run

--- fetch.py
+++ fetch.py
@@ -7,7 +7,9 @@ A simple script which checks the twitter account of uberspace
 specific keywords.
 """
 
+import hashlib
 import html
+import os
 import re
 import requests
 import subprocess
@@ -18,6 +20,9 @@ RELEVANT_HOSTS = [
 #NOTIFY_COMMAND = 'cat; echo subject: {}'
 NOTIFY_COMMAND = 'mail -s "{}" $USER'
 URL = 'https://twitter.com/ubernauten'
+
+# hashes of all messages stored in STORE_FILE to prevent duplicates
+STORE_FILE = os.path.join(os.getcwd(), 'notified')
 
 
 def main():
@@ -41,12 +46,34 @@ def get_messages():
 
 def send_notification(host: str, message: str):
     """Send a notification message."""
+    if check_for_done_notification(message):
+        # notification has already been sent
+        return
     subject = 'New message regarding {}'.format(host)
     subprocess.Popen(
         NOTIFY_COMMAND.format(subject), shell=True,
         stdin=subprocess.PIPE).communicate(message.encode())
 
+def check_for_done_notification(message: str) -> bool:
+    """Check whether a notification message has been sent already and store
+    the hash."""
+    hash = hashlib.sha256(message.encode()).hexdigest()
+    if hash in get_stored_hashes():
+        return True
+    add_stored_hash(hash)
+    return False
 
+
+def add_stored_hash(hash: str):
+    with open(STORE_FILE, 'a') as file:
+        file.write('{}\n'.format(hash))
+
+
+def get_stored_hashes() -> list:
+    if not os.path.isfile(STORE_FILE):
+        return []
+    with open(STORE_FILE, 'r') as file:
+        return file.read().split('\n')
 
 def check_relevance(message: str) -> str:
     """Check if a message is relevant and return host."""
